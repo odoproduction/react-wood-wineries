ca3e4531cdcf7b585ba0e9fcda0e3f7903a3d4e7
Karen Nahra <karen@odoproduction.com>
2022-08-17 19:09:42 -0700

    save for tonight

--- src/components/Winery.jsx
+++ src/components/Winery.jsx
@@ -1,13 +1,16 @@
 const Winery = (props) => {
 console.log(props)
-  return<div className="card">
+  return <div className="card">
     <li className="container">
       <img src={props.logo} />
       <p><strong>District: </strong>{props.district}</p>
       <p>{props.name}</p>
       <p>{props.favorite}</p>
-      <p>{props.location.city}</p>
-      <p>{props.phone}</p>
+      <ul>
+        <li>{props.location.street}</li>
+        <li>{props.location.city}, {props.location.state} {props.location.postcode}</li>
+        <li>{props.phone}</li>
+      </ul>
       <a href={props.web_link} >{props.website}</a>
     </li>
   </div>
